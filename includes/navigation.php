3bfd61e8eec4cd8a8a0f80067236e7bc494025d5
Jonas <git@grujo.ch>
2019-01-27 19:31:24 +0100

    Test

--- includes/navigation.php
+++ includes/navigation.php
@@ -3,4 +3,8 @@
 	<a href="team.php" <?=($page_name=="Ihr Team"?'class="active"':'')?>>Ihr Team</a>
 	<a href="infos.php" <?=($page_name=="Informationen"?'class="active"':'')?>>Informationen<span class="hide-on-mobile"> zu Ihrem Aufenthalt<span></a>
 	<a href="kontakt.php" <?=($page_name=="Kontakt"?'class="active"':'')?>>Kontakt</a>
+
+	<a href="buchen.php" id="buchen">Buchen</a>
+	<a href="login.php" class="login<?=($page_name=="Login"?' active':'')?>">Login</a>
+
 </nav>
