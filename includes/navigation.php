fdc10bab1b74c29d093483e02e5e059cfcc0692e
grujo <570617+jgrueter@users.noreply.github.com>
2019-03-26 07:22:24 +0100

    Update navigation.php

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,18 +1,17 @@
 <div class="nav-wrapper">
 	<div class="container">
 		
-    <div class="pull-left logo">
+		<div class="pull-left logo">
 			<a href="index.php"><img src="img/logo.svg" alt="Logo" class="hide-on-mobile"></a>
 			<a href="index.php"><img src="img/logo-klein.svg" alt="Logo" class="show-on-mobile"></a>
 		</div>
 		
-	  <nav class="navigation pull-right">
-	    <a href="index.php" <?=($page_name=="Home"?'class="active"':'')?>>Home</a>
-	    <a href="team.php" <?=($page_name=="Ihr Team"?'class="active"':'')?>><span class="hide-on-mobile">Ihr Team</span><span class="show-on-mobile">Team</span></a>
-	    <a href="infos.php" <?=($page_name=="Informationen"?'class="active"':'')?>><span class="hide-on-mobile">Informationen</span><span class="show-on-mobile">Infos</span></a>
-	    <a href="kontakt.php" <?=($page_name=="Kontakt"?'class="active"':'')?>>Kontakt</a>
-	    <a href="#modal-nav" class="navbar-toggle uk-navbar-content" data-modal-toggle="#modal-nav"></a>
-	  </nav>
+		<nav class="navigation pull-right">
+			<a href="index.php" <?=($page_name=="Home"?'class="active"':'')?>>Home</a>
+			<a href="team.php" <?=($page_name=="Ihr Team"?'class="active"':'')?>><span class="hide-on-mobile">Ihr Team</span><span class="show-on-mobile">Team</span></a>
+			<a href="infos.php" <?=($page_name=="Informationen"?'class="active"':'')?>><span class="hide-on-mobile">Informationen</span><span class="show-on-mobile">Infos</span></a>
+			<a href="kontakt.php" <?=($page_name=="Kontakt"?'class="active"':'')?>>Kontakt</a>
+		</nav>
 	</div> <!-- Container -->
 </div> <!-- Nav Wrapper -->
 
